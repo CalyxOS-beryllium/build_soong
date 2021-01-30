8e6262e96f558c16673cb5806623af9714d6a16e
Chirayu Desai <chirayudesai1@gmail.com>
2021-01-30 20:08:32 +0530

    Replace generated_kernel_headers with device_kernel_headers when not building inline
    Change-Id: I01d4a9b3e24315731efbc8d16882818d20e38e89

--- cc/cc.go
+++ cc/cc.go
@@ -2099,6 +2099,23 @@ func (c *Module) DepsMutator(actx android.BottomUpMutatorContext) {
 		for idx, lib := range deps.RuntimeLibs {
 			deps.RuntimeLibs[idx] = rewriteSnapshotLib(lib, getSnapshot().SharedLibs)
 		}
+
+		rewriteHeaderLibs := func(list []string) (newHeaderLibs []string) {
+			newHeaderLibs = []string{}
+			for _, entry := range list {
+				// Replace generated_kernel_headers with device_kernel_headers
+				// when not building inline
+				if entry == "generated_kernel_headers" {
+					if (ctx.Config().Getenv("INLINE_KERNEL_BUILDING") != "true") {
+						newHeaderLibs = append(newHeaderLibs, "device_kernel_headers")
+						continue
+					}
+				}
+				newHeaderLibs = append(newHeaderLibs, entry)
+			}
+			return newHeaderLibs
+		}
+		deps.HeaderLibs = rewriteHeaderLibs(deps.HeaderLibs)
 	}
 
 	for _, lib := range deps.HeaderLibs {
